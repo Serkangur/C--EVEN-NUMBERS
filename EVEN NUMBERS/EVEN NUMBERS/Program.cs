8e2519ad0d959cf9a6af2d3831ce2f753511022d
Serkan <94819174+Serkangur@users.noreply.github.com>
2021-11-22 16:09:11 +0300

    C# EVEN NUMBERS
    C# EVEN NUMBERS

---
+++ EVEN NUMBERS/EVEN NUMBERS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVEN_NUMBERS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 0; a <= 10; a = a + 2)
+            {
+                Console.WriteLine(a);
+            }
+            Console.ReadLine();
+        }
+    }
+}
